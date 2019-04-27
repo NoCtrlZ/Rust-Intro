d32e1185998d2455b0efdaa0567fa5c881e35d74
Hemlocked <phantomofrotten@gmail.com>
2019-04-27 10:57:22 +0900

    半径と円周率を引数にしてprintする

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
-fn hello(){
-    println!("Hello, world!");
-}
-
 fn main() {
-    println!("Hello, world!");
-    hello();
+    println!("
+    半径 {:.1}、 円周率 {:.3}、 面積 {:.3}",
+    // 半径
+    3.2,
+    // 円周率
+    std::f64::consts::PI,
+    // 半径 * 円周率
+    3.2f64.powi(2) * std::f64::consts::PI,
+    );
 }
